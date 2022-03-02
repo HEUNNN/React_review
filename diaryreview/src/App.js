0b63d7fbe07afb2711971163589c2183f2850457
leehyeeun <leehe0430@gmail.com>
2022-03-03 00:01:42 +0900

    2022/3/2 diary review Optimization 4 useReducer

--- diaryreview/src/App.js
+++ diaryreview/src/App.js
@@ -1,14 +1,50 @@
-import { useEffect, useRef, useState, useMemo, useCallback } from "react";
+import {
+  useEffect,
+  useRef,
+  useState,
+  useMemo,
+  useCallback,
+  useReducer,
+} from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
+const dataReducer = (state, action) => {
+  switch (action.type) {
+    case "INIT": {
+      return action.data; // 현재 state 업데이트
+    }
+    case "CREATE": {
+      const created_date = new Date().getTime();
+      const newItem = {
+        ...action.data, // ... 은 펼쳐서 놓기
+        created_date,
+      };
+      return [newItem, ...state];
+    }
+    case "DELETE": {
+      const deleteData = state.filter((it) => it.id !== action.data);
+      return deleteData;
+    }
+    case "EDIT": {
+      const editData = state.map((it) =>
+        it.id === action.targetId ? { ...it, content: action.newContent } : it
+      );
+      return editData;
+    }
+    default:
+      return state;
+  }
+};
 
 function App() {
-  const [data, setData] = useState([]); //데이터를 관리할 state(최상단에 위치)
+  //const [data, setData] = useState([]); //데이터를 관리할 state(최상단에 위치)
+  const [data, dispatch] = useReducer(dataReducer, []);
   const dataId = useRef(0); //id 값을 0 으로 정의(초기값)
 
   //JSON Placeholder에서 데이터를 받아올 것이다. = React에서 API 호출
   const getData = async () => {
+    //init action
     //async를 붙여서 getData가 promise 객체를 반환하도록 한다.
     const res = await fetch(
       "https://jsonplaceholder.typicode.com/comments"
@@ -22,7 +58,7 @@ function App() {
         id: dataId.current++, //return 되니까 후위 연산자 사용
       };
     });
-    setData(initData);
+    dispatch({ type: "INIT", data: initData });
   };
   useEffect(() => {
     // 처음 mount 될 때 getDate() 호출
@@ -30,34 +66,22 @@ function App() {
   }, []);
   //기존의 일기 배열에 새로운 일기를 추가하는 함수: onCreate
   const onCreate = useCallback((author, content, emotion) => {
+    //create action
     //새로운 데이터를 받아올 parameter
-    const created_date = new Date().getTime();
-    const newItem = {
-      author,
-      content,
-      emotion,
-      created_date,
-      id: dataId.current,
-    };
+    dispatch({
+      type: "CREATE",
+      data: { author, content, emotion, id: dataId.current }, //creared_date는 리듀서에서 만들예정
+    });
     dataId.current += 1; //추가될 데이터의 id 값이 1씩 증가됨
-    setData((data) => {
-      return [newItem, ...data];
-    }); //함수형 업데이트
   }, []);
   const onDelete = useCallback((targetId) => {
-    setData((data) => data.filter((it) => it.id !== targetId)); //setData()를 통해 data 상태를 변경(갱신)할 수 있음
+    // delete action
+    dispatch({ type: "DELETE", data: targetId });
   }, []);
   //무엇을 수정할지 모르고 그저 props해주기 때문에 targetId parameter 필요
   const onEdit = useCallback((targetId, newContent) => {
-    setData(
-      (data) =>
-        data.map((elem) =>
-          elem.id === targetId ? { ...elem, content: newContent } : elem
-        )
-
-      //elem.id !== targetId 이면 그냥 elem을 반환,
-      //elem.id === targetId 이면, 수정된 content로 교체된 elem을 setDate()에 전달해주어서 data 상태를 변화시킴
-    );
+    //edit action
+    dispatch({ type: "EDIT", targetId, newContent });
   }, []);
   const getDiaryAnalysis = useMemo(() => {
     const goodCount = data.filter((v) => v.emotion >= 3).length;
